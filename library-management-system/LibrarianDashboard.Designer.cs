4bccb25793e08d93612008cb087d24c3ecc83ebb
Kavindu Sanjula <ravanadevs@gmail.com>
2024-02-10 22:31:25 +0530

    dashboard style

--- library-management-system/LibrarianDashboard.Designer.cs
+++ library-management-system/LibrarianDashboard.Designer.cs
@@ -92,7 +92,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(6, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(248, 494);
+            this.groupBox1.Size = new System.Drawing.Size(248, 479);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Add Book";
@@ -170,7 +170,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1030, 506);
+            this.tabPage2.Size = new System.Drawing.Size(1030, 491);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Users";
             this.tabPage2.UseVisualStyleBackColor = true;
